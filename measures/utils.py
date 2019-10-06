b95aece65a59df78c09eb016c34ea91306c2f1f3
akki2825 <akhilesh.rocking@gmail.com>
2019-10-06 19:14:43 +0530

    add(measures/utils.py): add file

---
+++ measures/utils.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def _average_statistic(statistic, sentence_graphs):
+    """Calculate the statistic for every sentence and return mean and
+    standard deviation.
+    """
+    results = [statistic(g) for g in sentence_graphs]
+    return np.mean(results), np.std(results)
