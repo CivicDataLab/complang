2be539cb9ca727eb7e2aeba41aa8c09ada6ce83d
akki2825 <akhilesh.rocking@gmail.com>
2019-10-07 00:56:52 +0530

    edit(measures/vocab_based.py): add type hinting

--- measures/vocab_based.py
+++ measures/vocab_based.py
@@ -1,48 +1,47 @@
+from typing import List
 import collections
 import numpy as np
 
-# TODO: Add type hinting
 
-def type_token_ratio(txt_len, vocab_size):
+def type_token_ratio(txt_len: int, vocab_size: int) -> float:
     return vocab_size / txt_len
 
-def guiraud_r(txt_len, vocab_size):
+def guiraud_r(txt_len: int, vocab_size: int) -> np.float64:
     return vocab_size / np.sqrt(txt_len)
 
-def herdan_c(txt_len, vocab_size):
+def herdan_c(txt_len: int, vocab_size: int) -> np.float64:
     return np.log(vocab_size) / np.log(txt_len)
 
-def dugast_k(txt_len, vocab_size):
+def dugast_k(txt_len: int, vocab_size: int) -> np.float64:
     return np.log(vocab_size) / np.log(np.log(txt_len))
 
-def maas_a2(txt_len, vocab_size):
+def maas_a2(txt_len, vocab_size) -> np.float64:
     return (np.log(txt_len) - np.log(vocab_size)) / (np.log(txt_len) ** 2)
 
-def dugast_u(txt_len, vocab_size):
+def dugast_u(txt_len: int, vocab_size: int) -> np.float64:
     return (np.log(txt_len) ** 2) / (np.log(txt_len) - np.log(vocab_size))
 
 
-def tuldava_ln(txt_len, vocab_size):
+def tuldava_ln(txt_len: int, vocab_size: int) -> np.float64:
     return (1 - (vocab_size ** 2)) / ((vocab_size ** 2) * np.log(txt_len))
 
-
-def brunet_w(txt_len, vocab_size):
+# Check
+def brunet_w(txt_len: int, vocab_size: int) -> float:
     """Brunet (1978)"""
     a = -0.172
-    return txt_len ** (vocab_size ** -a)  # Check
+    return txt_len ** (vocab_size ** -a)
 
-
-def cttr(txt_len, vocab_size):
+def cttr(txt_len: int, vocab_size: int) -> np.float64:
     """Carroll's Corrected Type-Token Ration"""
     return vocab_size / np.sqrt(2 * txt_len)
 
 
-def summer_s(txt_len, vocab_size):
+def summer_s(txt_len: int, vocab_size: int) -> np.float64:
     """Summer's S index"""
-    return np.log(np.log(vocab_size)) / np.log(math.log(txt_len))
+    return np.log(np.log(vocab_size)) / np.log(np.log(txt_len))
 
 
-def sttr(tokens, window_size=1000, ci=False):
+def sttr(tokens: List[str], window_size: int = 1000, ci: bool=False) -> np.float64:
     """calculate standardized type-token ratio
     originally Kubat&Milicka 2013. Much better explained
     in Evert et al. 2017.
