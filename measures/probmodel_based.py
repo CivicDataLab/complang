2c86e94cb2f01c0622590e272c94036f411e9aae
akki2825 <akhilesh.rocking@gmail.com>
2019-11-04 13:24:04 +0530

    add type annotations

--- measures/probmodel_based.py
+++ measures/probmodel_based.py
@@ -1,17 +1,22 @@
 import numpy as np
+from typing import Dict
 
-# TODO: Add type hinting
 
-
-def orlov_z(txt_len, vocab_size, freq_spectrum, max_iterations=100, min_tolerance=1):
+def orlov_z(
+    txt_len: int,
+    vocab_size: int,
+    freq_spectrum: Dict,
+    max_iterations=100,
+    min_tolerance=1,
+):
     """Orlov (1983)
 
     Approximation via Newton's method.
     """
 
     def function(txt_len, vocab_size, p_star, z):
-        return (z / np.log(p_star * z)) * (txt_len / (text_length - z)) * np.log(
-            text_length / z
+        return (z / np.log(p_star * z)) * (txt_len / (txt_len - z)) * np.log(
+            txt_len / z
         ) - vocab_size
 
     def derivative(txt_len, vocab_size, p_star, z):
@@ -21,11 +26,10 @@ def orlov_z(txt_len, vocab_size, freq_spectrum, max_iterations=100, min_toleranc
         return (
             txt_len
             * (
-                (z - text_length) * np.log(p_star * z)
-                + np.log(text_length / z)
-                * (text_length * np.log(p_star * z) - text_length + z)
+                (z - txt_len) * np.log(p_star * z)
+                + np.log(txt_len / z) * (txt_len * np.log(p_star * z) - txt_len + z)
             )
-        ) / (((text_length - z) ** 2) * (np.log(p_star * z) ** 2))
+        ) / (((txt_len - z) ** 2) * (np.log(p_star * z) ** 2))
 
     most_frequent = max(freq_spectrum.keys())
     p_star = most_frequent / txt_len
@@ -33,12 +37,12 @@ def orlov_z(txt_len, vocab_size, freq_spectrum, max_iterations=100, min_toleranc
     for i in range(max_iterations):
         next_z = z - (
             function(txt_len, vocab_size, p_star, z)
-            / derivative(text_length, vocabulary_size, p_star, z)
+            / derivative(txt_len, vocab_size, p_star, z)
         )
         abs_diff = abs(z - next_z)
         z = next_z
         if abs_diff <= min_tolerance:
             break
     else:
-        warnings.warn("Exceeded max_iterations")
+        print("Exceeded max_iterations")
     return z
